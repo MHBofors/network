fc95263a9383281dd3dad5af387b49798c1a44b9
MHBofors <markus@bovik.se>
2026-06-19 13:18:20 +0200

    Format

--- inet_socket.c
+++ inet_socket.c
@@ -1,68 +1,81 @@
 #include <arpa/inet.h>
+#include <errno.h>
+
 #include <netinet/in.h>
 #include <unistd.h>
-#include <errno.h>
+
 
 #include "inet_socket.h"
 
-int inet_connect(const char *host, const char *service, int type) {
-	struct addrinfo hints = {0};
-	struct addrinfo *ai_list, *ai_node;
-	int sockfd;
+int inet_connect(const char *host,
+				 const char *service,
+				 int		 type) {
+	struct addrinfo *ai_list = NULL;
+	struct addrinfo *ai_node = NULL;
+	struct addrinfo	 hints	 = {.ai_canonname = NULL,
+								.ai_addr	  = NULL,
+								.ai_next	  = NULL,
+								.ai_family	  = AF_UNSPEC,
+								.ai_socktype  = type};
+	int				 sockfd	 = -1;
 
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = type;
-
-	if(getaddrinfo(host, service, &hints, &ai_list) != 0) {
+	if (getaddrinfo(host, service, &hints, &ai_list) != 0) {
 		errno = ENOSYS;
 		return -1;
 	}
 
-	for(ai_node = ai_list; ai_node != NULL; ai_node = ai_node->ai_next) {
-		if((sockfd = socket(ai_node->ai_family, ai_node->ai_socktype, ai_node->ai_protocol)) == -1) 
-			continue; /* ON ERROR */
+	for (ai_node = ai_list; ai_node != NULL; ai_node = ai_node->ai_next) {
+		/* ON ERROR */
+		if ((sockfd = socket(ai_node->ai_family,
+							 ai_node->ai_socktype,
+							 ai_node->ai_protocol)) == -1)
+			continue;
 
-		if(connect(sockfd, ai_node->ai_addr, ai_node->ai_addrlen) != -1)
-			break; /* ON SUCCESS */
 
-		close(sockfd); /* CLOSE SOCKET ON FAILED CONNECTION */
+		/* ON SUCCESS */
+		if (connect(sockfd, ai_node->ai_addr, ai_node->ai_addrlen) != -1)
+			break;
+
+		/* CLOSE SOCKET ON FAILED CONNECTION */
+		close(sockfd);
 	}
 
 	freeaddrinfo(ai_list);
-
 	return (ai_node == NULL) ? -1 : sockfd;
 }
 
-int inet_bind(const char *host, const char *service, int type) {
-	struct addrinfo hints = {0};
-	struct addrinfo *ai_list, *ai_node;
-	int sockfd;
+int inet_bind(const char *host,
+			  const char *service,
+			  int		  type) {
+	struct addrinfo *ai_list = NULL;
+	struct addrinfo *ai_node = NULL;
+	struct addrinfo	 hints	 = {.ai_canonname = NULL,
+								.ai_addr	  = NULL,
+								.ai_next	  = NULL,
+								.ai_family	  = AF_UNSPEC,
+								.ai_socktype  = type};
+	int				 sockfd	 = -1;
 
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = type;
-
-	if(getaddrinfo(host, service, &hints, &ai_list) != 0) {
+	if (getaddrinfo(host, service, &hints, &ai_list) != 0) {
 		errno = ENOSYS;
 		return -1;
 	}
 
-	for(ai_node = ai_list; ai_node != NULL; ai_node = ai_node->ai_next) {
-		if((sockfd = socket(ai_node->ai_family, ai_node->ai_socktype, ai_node->ai_protocol)) == -1) 
-			continue; /* ON ERROR */
+	for (ai_node = ai_list; ai_node != NULL; ai_node = ai_node->ai_next) {
+		/* ON ERROR */
+		if ((sockfd = socket(ai_node->ai_family,
+							 ai_node->ai_socktype,
+							 ai_node->ai_protocol)) == -1)
+			continue;
 
-		if(bind(sockfd, ai_node->ai_addr, ai_node->ai_addrlen) != -1)
-			break; /* ON SUCCESS */
+		/* ON SUCCESS */
+		if (bind(sockfd, ai_node->ai_addr, ai_node->ai_addrlen) != -1)
+			break;
 
-		close(sockfd); /* CLOSE SOCKET ON FAILED CONNECTION */
+		/* CLOSE SOCKET ON FAILED CONNECTION */
+		close(sockfd);
 	}
 
 	freeaddrinfo(ai_list);
-
 	return (ai_node == NULL) ? -1 : sockfd;
 }
--- inet_socket.h
+++ inet_socket.h
@@ -1,10 +1,14 @@
 #ifndef INET_SOCKET_H
 #define INET_SOCKET_H
 
-#include <sys/socket.h>
 #include <netdb.h>
+#include <sys/socket.h>
 
-int inet_connect(const char *host, const char *service, int type);
-int inet_bind(const char *host, const char *service, int type);
+int inet_connect(const char *host,
+				 const char *service,
+				 int		 type);
+int inet_bind(const char *host,
+			  const char *service,
+			  int		  type);
 
 #endif /* INET_SOCKET_H */
